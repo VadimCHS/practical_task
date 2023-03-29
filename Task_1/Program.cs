3ca928522ea5ccc0ee65e75ca405e7673a1230c0
Vadim <vadim-chernov-93@mail.ru>
2023-03-29 17:12:42 +0700

    Добавил ззадание №2

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,17 +1,21 @@
-﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-// a = 5    b = 7   -> max = 7
-// a = 2    b = 10  -> max = 10
-// a = -9   b = -3  -> max = -3
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2     3   7   -> 7
+// 44    5   78  -> 78
+// 22    3   9   -> 22
 
 
-Console.Write ("Число А = ");
-int numberA = Convert.ToInt32(Console.ReadLine ()); // Ввод числа А
-Console.Write ("Число В = ");
-int numberB = Convert.ToInt32(Console.ReadLine ()); // Ввод числа В
-if (numberA < numberB) {                            // Вычисление наибольшего и наименьшего числа
-    int n = numberA;                                    
-    numberA = numberB;
-    numberB = n;
+int[] numbers = new int[3];                             // Создание массива для хранения чисел
+string[] str = new string[3] {"А", "В", "С"};           // Массив обозначения чисел
+int max = 0;
+
+for (int i = 0; i < 3; i++){                            // Ввод чисел
+    Console.Write ($"Число {str[i]} = ");
+    numbers[i] = Convert.ToInt32(Console.ReadLine ());
 }
-Console.WriteLine ($"max = {numberA}");
-Console.WriteLine ($"min = {numberB}");
+
+for (int i = 0; i < 3; i++) {                           // Вычисление наибольшего
+    if (max < numbers[i]) max = numbers[i];
+}
+
+Console.WriteLine ($"max = {max}");
