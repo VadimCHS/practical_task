e1c0545062d3070e01d7c7e175258f99cc8d9475
Vadim <vadim-chernov-93@mail.ru>
2023-04-05 17:56:56 +0700

    Семинар 4

---
+++ Seminar_4/Task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int ReadNumberInt(string msg)                           // Ввод числа 
+{                                              
+        Console.Write(msg);
+        int result = Convert.ToInt32(Console.ReadLine());
+        return result;
+}
+
+int SumNumbers (int n){                                 // Расчет суммы цифр в числе
+    int result = 0;
+    for (int i = n; i != 0; i /= 10){
+        result += n % 10;
+        n /= 10;
+    }
+    return result;
+}
+
+int n = ReadNumberInt ("Введите число: ");
+Console.WriteLine ($"{n} -> {Math.Abs(SumNumbers (n))}");// Вывод суммы цифр в числе
---
+++ Seminar_4/Task_3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int ReadNumberInt(string msg)                           // Ввод числа 
+{                                              
+        Console.Write(msg);
+        int result = Convert.ToInt32(Console.ReadLine());
+        return result;
+}
+
+int RandomNumberInt (int a, int b)                       // Случайное число с заданием диапозона
+{                                              
+        int result = new Random().Next(a, b + 1);
+        return result;
+}
+
+Console.WriteLine ("Введите диапозон заполнения массива: ");
+int a = ReadNumberInt("От -> ");
+int b = ReadNumberInt("До -> ");
+int[] arr = new int[8];
+for (int i = 0; i < arr.Length; i++){                      // Заполнение массива   
+    arr[i] = RandomNumberInt (a, b);
+}
+
+Console.WriteLine ($"{String.Join(", ", arr)} -> [{String.Join(",",arr)}]"); // Вывод массива
+
