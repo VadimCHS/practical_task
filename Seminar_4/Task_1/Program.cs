82b51b7b018493a9082255035be52af82904c101
Vadim <vadim-chernov-93@mail.ru>
2023-04-05 17:56:15 +0700

    Семинар 4

---
+++ Seminar_4/Task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int ReadNumberInt(string msg)                           // Ввод числа 
+{                                              
+        Console.Write(msg);
+        int result = Convert.ToInt32(Console.ReadLine());
+        return result;
+}
+
+double Exponentiate (double a, int b){                        // Возведение числа А в степень В
+    double result = 1;
+    if (b == 0) return result;
+    else {
+        for (int i = 1; i <= b ; i++){
+        result *= a;
+        }
+        return result;
+    }
+}
+
+int a = ReadNumberInt ("Введите число: ");
+int b = ReadNumberInt ("Введите степень: ");
+Console.WriteLine ($" в степени  = {Exponentiate(a, b)}");
