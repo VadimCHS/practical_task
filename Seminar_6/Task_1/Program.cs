feea3a6f0193716f4d276727710e389a09828605
Vadim <vadim-chernov-93@mail.ru>
2023-04-16 22:40:07 +0700

    Семинар 6

---
+++ Seminar_6/Task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.Clear();
+
+int[] GetArrFromStr (string strArr)
+{
+    string[] nums = strArr.Split(" ");
+    int[] res = new int[nums.Length];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+
+int Pos (int[] n)
+{
+    int count = 0;
+    foreach (var el in n)
+    {
+        count += el > 0 ? 1: 0;
+    }
+    return count;
+}
+
+Console.WriteLine ("Введите элементы массива через пробел: ");
+string elements = Console.ReadLine();
+int[] n = GetArrFromStr(elements);
+Console.Write ($"[{String.Join(", ", n)}] -> {Pos(n)}"); 
---
+++ Seminar_6/Task_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.Clear();
+double[] IntersectionPoint (double[] n)
+{
+    double[] intPoint = new double[2];
+    intPoint[0] = Math.Round((n[0]-n[2])/(n[3]-n[1]), 2);
+    intPoint[1] = Math.Round(n[3]*intPoint[0]+n[2], 2);
+    return intPoint;
+}
+
+double[] GetArrFromStr (string strArr)
+{
+    string[] nums = strArr.Split(" ");
+    double[] res = new double[nums.Length];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+
+Console.Write ("Введите значения b1, k1, b2, k2 через пробел: ");
+string elements = Console.ReadLine();
+double[] coord = GetArrFromStr(elements);
+Console.Write ($"[{String.Join(", ", coord)}] -> ({String.Join("; ", IntersectionPoint(coord))})"); 
